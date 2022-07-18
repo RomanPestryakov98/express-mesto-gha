b7faf2c7ac699bf2da34a394b7c1e09ce62dc8fa
RomanPestryakov98 <95371899+RomanPestryakov98@users.noreply.github.com>
2022-07-19 00:45:41 +0300

    4.6

--- controllers/not-found.js
+++
@@ -1,4 +0,0 @@
-module.exports.getError = (req, res) => {
-  console.log('!@');
-  res.status(404).send('123');
-};
--- routes/not-found.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const { getError } = require('../controllers/not-found');
-
-router.get('/', getError);
-
-module.exports = router;
