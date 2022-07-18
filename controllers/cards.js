a16996ed87da8b48e88fd16d79daf5a632e06840
RomanPestryakov98 <95371899+RomanPestryakov98@users.noreply.github.com>
2022-07-18 22:01:44 +0300

    4.4

--- controllers/cards.js
+++ controllers/cards.js
@@ -21,9 +21,17 @@ module.exports.createCards = (req, res) => {
 
 module.exports.deleteCard = (req, res) => {
   Card.findByIdAndRemove(req.params.id)
-    .then((card) => res.send(card))
+    .then((card) => {
+      if (!card) {
+        return Promise.reject(new Error('Карточки не существует'));
+      }
+      return res.send(card);
+    })
     .catch((err) => {
       if (err.name === 'CastError') {
+        return res.status(400).send({ message: 'Некорректный id карточки' });
+      }
+      if (err.message === 'Карточки не существует') {
         return res.status(404).send({ message: 'Карточка не найдена' });
       }
       return res.status(500).send({ message: 'Неизвестная ошибка' });
@@ -36,10 +44,18 @@ module.exports.likeCard = (req, res) => {
     { $addToSet: { likes: req.user._id } },
     { new: true },
   )
-    .then((card) => res.send(card))
+    .then((card) => {
+      if (!card) {
+        return Promise.reject(new Error('Карточки не существует'));
+      }
+      return res.send(card);
+    })
     .catch((err) => {
       if (err.name === 'CastError') {
-        return res.status(404).send({ message: 'Передан несуществующий _id карточки' });
+        return res.status(400).send({ message: 'Передан несуществующий _id карточки' });
+      }
+      if (err.message === 'Карточки не существует') {
+        return res.status(404).send({ message: 'Карточка не найдена' });
       }
       return res.status(500).send({ message: 'Неизвестная ошибка' });
     });
@@ -51,10 +67,18 @@ module.exports.dislikeCard = (req, res) => {
     { $pull: { likes: req.user._id } },
     { new: true },
   )
-    .then((card) => res.send(card))
+    .then((card) => {
+      if (!card) {
+        return Promise.reject(new Error('Карточки не существует'));
+      }
+      return res.send(card);
+    })
     .catch((err) => {
       if (err.name === 'CastError') {
-        return res.status(404).send({ message: 'Передан несуществующий _id карточки' });
+        return res.status(400).send({ message: 'Передан несуществующий _id карточки' });
+      }
+      if (err.message === 'Карточки не существует') {
+        return res.status(404).send({ message: 'Карточка не найдена' });
       }
       return res.status(500).send({ message: 'Неизвестная ошибка' });
     });
